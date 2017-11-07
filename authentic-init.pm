7175309eecbc15dc4a1049dc0a448dec3ebe1e77
Ilia Rostovtsev <ilia@rostovtsev.ru>
2017-11-07 13:51:00 +0300

    19.02

--- authentic-init.pm
+++ authentic-init.pm
@@ -866,8 +866,8 @@ sub theme_git_version
 sub theme_version
 {
     my ($switch)            = @_;
-    my $sh__ln__p___version = '19.00';
-    my $sh__ln__c___version = '19.01';
+    my $sh__ln__p___version = '19.01';
+    my $sh__ln__c___version = '19.02';
     my $sh__ln__g___version = theme_git_version('uncond');
     ((!$switch && $sh__ln__g___version) &&
      ($sh__ln__c___version = $sh__ln__g___version, ($sh__ln__c___version =~ s/\.|-|git//ig)));
