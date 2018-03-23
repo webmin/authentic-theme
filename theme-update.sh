72bd06b4e01945eecf4ecffcc6331c49715bed4b
Ilia Rostovtsev <ilia@rostovtsev.ru>
2018-03-23 15:31:43 +0300

    Add ability to automatically pull latest Webmin/Usermin update script

--- theme-update.sh
+++ theme-update.sh
@@ -59,6 +59,17 @@ else
     # Require `git` command availability
     if type ${GIT} >/dev/null 2>&1
     then
+
+      # Grab latest version of the update script for Webmin/Usermin
+      if type ${CURL} >/dev/null 2>&1
+      then
+        curl -s -o $DIR/update-from-repo.sh https://raw.githubusercontent.com/webmin/webmin/master/update-from-repo.sh
+        chmod +x $DIR/update-from-repo.sh
+      else
+        echo -e "\e[49;0;33;82mError: Command \`curl\` is not installed or not in the \`PATH\`.\e[0m";
+        exit
+      fi
+
       # Pull latest changes
       if [[ "$1" == *"-release"* ]]; then
         if [[ "$1" == *":"* ]] && [[ "$1" != *"latest"* ]]; then
@@ -90,8 +101,9 @@ else
 
       # Check version compatibility
       if [ -z ${WEBMIN_CONFIG} ]; then
-        DVER=$(grep -Po '(?<=^depends=).*$' "$DIR/.~authentic-theme/theme.info")
-        TVER=$(grep -Po '(?<=^version=).*$' "$DIR/.~authentic-theme/theme.info")
+        DVER=$(grep -Po '(?<=^depends=).*$' "$DIR/.~authentic-theme/theme.info" 2>&1)
+        TVER=$(grep -Po '(?<=^version=).*$' "$DIR/.~authentic-theme/theme.info" 2>&1)
+        GRPERR=$?
         PVER=`head -n 1 $DIR/version`
         RVER=$(echo "$DVER" | cut -d ' ' -f 1)
 
@@ -102,9 +114,9 @@ else
             RVER=0
           fi
         fi
-
-        if [[ $RVER > $PVER ]]; then
-          echo -e "
+        if [ $GRPERR  -eq 0 ]; then
+          if [[ $RVER > $PVER ]]; then
+            echo -e "
 \e[47;1;31;82mWarning!\e[0m Installing this version of \e[49;1;37;182mAuthentic Theme\e[0m \e[49;32;5;82m"${TVER^}"\e[0m requires
 latest and/or possibly unreleased version of \e[49;1;37;182m"${PROD^}"\e[0m \e[49;32;5;82m"${RVER^}"\e[0m, to work
 properly. Your current version of \e[49;1;37;182m"${PROD^}"\e[0m is \e[5;49;33m"${PVER^}"\e[0m. There might be
@@ -112,12 +124,13 @@ incompatible changes, that could stop the theme from working as designed.
 It is recommended to upgrade \e[49;1;37;182m"${PROD^}"\e[0m to the latest development version, by
 running \e[3m\`update-from-repo.sh\`\e[0m script from \e[3m\`"$DIR"\`\e[0m directory."
 
-          read -p "Do you want to continue to force install the theme anyway? [y/N] " -n 1 -r
-          echo
-          if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-            rm -rf "$DIR/.~authentic-theme"
-            echo -e "\e[49;1;35;82mOperation aborted.\e[0m"
-            exit
+            read -p "Do you want to continue to force install the theme anyway? [y/N] " -n 1 -r
+            echo
+            if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+              rm -rf "$DIR/.~authentic-theme"
+              echo -e "\e[49;1;35;82mOperation aborted.\e[0m"
+              exit
+            fi
           fi
         fi
       fi
@@ -130,14 +143,16 @@ running \e[3m\`update-from-repo.sh\`\e[0m script from \e[3m\`"$DIR"\`\e[0m direc
         rm -rf "$DIR/.~authentic-theme"
 
         # Local clear
-        if [ -f "$DIR/authentic-theme/.debug.pm" ]; then
-          rm -f "$DIR/authentic-theme/.debug.pm"
-        fi
         rm -f "$DIR/authentic-theme/README.md"
         find "$DIR/authentic-theme/" -name "*.src*" -delete
 
-        TVER=$(grep -Po '(?<=^version=).*$' "$DIR/authentic-theme/theme.info")
-        echo -e "\e[49;32;5;82mUpdating to Authentic Theme $TVER, done.\e[0m"
+        TVER=$(grep -Po '(?<=^version=).*$' "$DIR/authentic-theme/theme.info" 2>&1)
+        GRPERR2=$?
+        if [ $GRPERR2 -eq 0 ]; then
+          echo -e "\e[49;32;5;82mUpdating to Authentic Theme $TVER, done.\e[0m"
+        else
+          echo -e "\e[49;32;5;82mUpdating to the latest Authentic Theme, done.\e[0m"
+        fi
 
         # Restart Webmin/Usermin in case it's running
         if [ "$2" != "-no-restart" ]; then
