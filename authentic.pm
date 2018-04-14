5dfe7bf342b7b8f9d82e492035fac705c73b96a7
Ilia Rostovtsev <ilia@rostovtsev.ru>
2018-04-14 14:38:17 +0300

    Fix defaulting to `POST` method for submitting forms https://github.com/webmin/webmin/issues/893#issuecomment-379585204

--- authentic.pm
+++ authentic.pm
@@ -450,11 +450,8 @@ sub theme_ui_form_start
 
     $rv .= '<form class="ui_form" ';
     $rv .= 'action="' . &html_escape($script) . '" ';
-
-    #$rv .= ($method eq 'form-data' ? 'method="post" enctype="multipart/form-data" ' : 'method="post" ');
-    $rv .= ($method eq 'post'        ? 'method="post" ' :
-              $method eq 'form-data' ? 'method="post" enctype="multipart/form-data" ' :
-              'method="get" ');
+    $rv .= ($method eq 'get' ? 'method="get" ' :
+              ($method eq 'form-data' ? 'method="post" enctype="multipart/form-data" ' : 'method="post" '));
     $rv .= ($target ? 'target="' . $target . '" ' : '');
     $rv .= ($tags   ? $tags                       : '');
     $rv .= '>' . "\n";
