bfe2cb2fa0e0eec32b4fcdab9b6dce8c899ef2f5
Jamie Cameron <jcameron@webmin.com>
2021-08-20 18:30:40 -0700

    Create links on install rather than putting links in the package

---
+++ postinstall.pl
@@ -0,0 +1,12 @@
+
+use WebminCore;
+init_config();
+
+sub module_install
+{
+foreach my $f ("manifest-cloudmin.json", "manifest-usermin.json",
+	       "manifest-webmin.json", "manifest-virtualmin.json") {
+	symlink_logged("$config_directory/authentic-theme/$f",
+		       "$root_directory/authentic-theme/$f");
+	}
+}
