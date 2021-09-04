4fb86719844257165d60e267cc8672bbb2766550
Ilia Rostovtsev <ilia@rostovtsev.io>
2021-09-04 13:42:36 +0300

    Fix formatting

--- postinstall.pl
+++ postinstall.pl
@@ -4,9 +4,13 @@ init_config();
 
 sub module_install
 {
-foreach my $f ("manifest-cloudmin.json", "manifest-usermin.json",
-	       "manifest-webmin.json", "manifest-virtualmin.json") {
-	symlink_logged("$config_directory/authentic-theme/$f",
-		       "$root_directory/authentic-theme/$f");
-	}
+    foreach my $f (
+    	"manifest-cloudmin.json",
+    	"manifest-usermin.json",
+    	"manifest-webmin.json",
+    	"manifest-virtualmin.json")
+    {
+        symlink_logged("$config_directory/authentic-theme/$f",
+        	           "$root_directory/authentic-theme/$f");
+    }
 }
