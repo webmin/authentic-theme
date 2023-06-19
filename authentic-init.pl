ceb6380648df0a513c6e1760c0b4b94ecc22809a
iliajie <gpg@ilia.engineer>
2023-06-20 02:06:25 +0300

    Add other UI improvements

--- authentic-init.pl
+++ authentic-init.pl
@@ -75,7 +75,7 @@ sub settings_filter
 
 sub get_theme_color
 {
-    my %theme_colors = ('blue'   => '#004192',
+    my %theme_colors = ('blue'   => '#004090',
                         'brown'  => '#5d4839',
                         'gold'   => '#7e6143',
                         'green'  => '#247648',
@@ -105,7 +105,7 @@ sub embed_favicon
     }
 
     my $theme_config_dir = "$config_directory/$current_theme";
-    my $theme_user_color = get_theme_color() || "#004192";
+    my $theme_user_color = get_theme_color() || "#004090";
 
     my $favicon_path = $theme_webprefix . '/images/favicons/' . $product_name;
     my $ref_link     = 'data-link-ref';
